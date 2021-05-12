12996351fe6fb3c81e96f2a9ae9a41ad39e1390c
HerrWolf <webmasterp2m@gmail.com>
2021-05-11 23:30:34 -0700

    first commit

---
+++ src/components/GifExpertApp.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { AddCategory } from './AddCategory';
+import { GifGrid } from './GifGrid';
+
+export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(['One Punch']);
+
+  return (
+    <>
+      <h2>GiftExpertApp</h2>
+      <AddCategory 
+        setCategories={setCategories}
+      />
+      <hr />
+
+      <ol>
+        {
+          categories.map(category => (
+            <GifGrid 
+              key={ category }
+              category={ category }
+            />
+          ))
+        }
+      </ol>
+    </>
+  )
+}
